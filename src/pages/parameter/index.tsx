9e42fe8ecae5b2ffccc0d67a995980b6c478d36f
Dimitri <dimitri.guitton.dev@gmail.com>
2024-01-24 10:48:23 +0100

    feat(parameter): template de la page

--- src/pages/parameter/index.tsx
+++ src/pages/parameter/index.tsx
@@ -1,8 +1,111 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import { Input } from '@/components/ui/input.tsx';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form.tsx';
+
+const formSchema = z.object({
+    api_key: z
+        .string()
+        .length(32, { message: 'La clé doit faire 32 caractères' }),
+    dropbox_directory: z.string(),
+});
+
 const ParameterPage = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            api_key: '',
+            dropbox_directory: '',
+        },
+    });
+
+    // 2. Define a submit handler.
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    };
+
     return (
-        <div>
-            <h1>Parameter Page</h1>
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Paramètres</CardTitle>
+                        <CardDescription>
+                            Gérer les paramètres de votre compte
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="api_key"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Ma clé d&apos;identification
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="eD5v2LWDFjcY1Sz4i0du4Jpp9S8zpvGF"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Cette clé est utilisée pour identifier
+                                        votre compte sur l&apos;ERP
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="dropbox_directory"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Mon dossier dropbox</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="John/Doe/MonDossier"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Ce dossier est utilisé pour stocker vos
+                                        fichiers sur Dropbox
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <Button type="submit">Mettre à jour</Button>
+                    </CardFooter>
+                </Card>
+            </form>
+        </Form>
     );
 };
 
