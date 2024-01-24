e7c9f900e0a1b459489cd35af97c3b04866210db
Dimitri <dimitri.guitton.dev@gmail.com>
2024-01-24 10:58:27 +0100

    feat(dashboard): mise en place du template

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,60 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card.tsx';
+
 const DashboardPage = () => {
     return (
-        <div>
-            <h1>Dashboard Page</h1>
-        </div>
+        <>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                <Card>
+                    <CardHeader>
+                        <CardDescription>Nouveau devis</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>35</h1>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardDescription>Devis validé</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>23</h1>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardDescription>Vente</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>35 000€</h1>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="my-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Statistiques</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>Graphique</h1>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Vente récentes</CardTitle>
+                        <CardDescription>Liste des ventes</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>Tableau</h1>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     );
 };
 
